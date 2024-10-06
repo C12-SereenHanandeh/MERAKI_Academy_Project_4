891c5723a99afd073f6b4a98dd212beec83ec95f
SereenHanandeh <sereen.hanandeh11@gmail.com>
2024-10-06 17:44:54 +0300

    login function

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const { sanitizeFilter } = require("mongoose");
 
 // Register a new user
-const registerUser = async (req, res) => {
+const register = async (req, res) => {
   const { name, email, password, role } = req.body;
   try {
     let user = await User.findOne({ email });
@@ -12,11 +12,18 @@ const registerUser = async (req, res) => {
       return res.status(400).json({ message: "User already exist" });
     }
 
+    const lowerCaseEmail = email.toLowerCase();
+
     // Generate salt and Hash the password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    user = new User({ name, email, password: hashedPassword, role });
+    user = new User({
+      name,
+      email: lowerCaseEmail,
+      password: hashedPassword,
+      role,
+    });
 
     await user.save();
 
@@ -34,3 +41,54 @@ const registerUser = async (req, res) => {
     res.status(500).send("server error");
   }
 };
+
+//----------------------------------------------------------------------------
+
+// Login a user
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // Find the user by email
+    const user = await User.findOne({ email });
+
+    // If user is not found, return 401 with an error message
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid login credentials",
+      });
+    }
+
+    // Compare the provided password with the stored hashed password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    // If passwords do not match, return 401 with an error message
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid login",
+      });
+    }
+
+    // Create a JWT payload
+    const payload = { user: { id: user.id, role: user.role } };
+
+    // Sign the JWT token
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    // Return a 200 status code with a success message and the token
+    return res.status(200).json({
+      success: true,
+      message: "Valid login",
+      token: token,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Server error");
+  }
+};
+
+module.exports = { register, login };
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const userRouter = express.Router();
 
-const { register } = require("../controllers/users");
+const { register, login } = require("../controllers/users");
 
 // Register a new user
 userRouter.post("/register", register);
+
+// login a user
+userRouter.post("/login", login);
 
 module.exports = userRouter;
